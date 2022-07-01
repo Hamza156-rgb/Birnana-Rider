f1425bac41b01f4fd448841e2324a1a0f53777b6
Hamza Waseem <69486753+Hamza156-rgb@users.noreply.github.com>
2022-07-01 13:33:54 +0500

    last

--- src/app/Components/sidemenu/sidemenu.component.ts
+++ src/app/Components/sidemenu/sidemenu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular'; 
 
 @Component({
   selector: 'app-sidemenu',
@@ -7,8 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidemenuComponent implements OnInit {
 
-  constructor() { }
+  constructor(private menu: MenuController) { }
 
   ngOnInit() {}
-
+  openEnd() {  
+    this.menu.close();
+   
+    }
 }
